f2466a3e157222623f5aa4c839c4fb52a4a014ec
myrajarenga <myrajarenga1234@gmail.com>
2023-02-28 06:46:30 -0800

    task3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_delete - function to delete an entire binary tree
+* @tree: pointer tpo the root node
+*Return: 0
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	
+	free(tree);
+}
+
