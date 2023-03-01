50b342bf73188ff79aa5e02c566bfedf9cfe033f
myrajarenga <myrajarenga1234@gmail.com>
2023-03-01 00:21:42 -0800

    task8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,6 +6,7 @@
 * @tree: pointer to the root node of the tree to traverse
 * @func: pointer to afucntion to call for each node
 */
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
