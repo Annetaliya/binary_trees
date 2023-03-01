08ec08a11d53afedc988e3e449c673b25beb5771
myrajarenga <myrajarenga1234@gmail.com>
2023-03-01 05:47:48 -0800

    task13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_nodes - function that counts thenodes with 1 child in abinary tree
+* @tree: pointer to root node of the tree to measure the number of nodes
+*
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if  (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		count++;
+	}
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+	
+	return (count);
+}
