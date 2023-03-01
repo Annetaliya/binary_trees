d9b778f9eca9365a7a0566377e906aa293d295ea
myrajarenga <myrajarenga1234@gmail.com>
2023-03-01 05:55:47 -0800

    task11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,7 +3,7 @@
 /**
 * binary_tree_size - function that mesures the depth of abinary tree
 * @tree: pointer to root node of the tree to measure the height
-*
+* Return: depth
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
@@ -11,6 +11,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
-	
