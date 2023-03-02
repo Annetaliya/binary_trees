1115a7cc676ed8e9c1bd39a54cef142d9d7c1778
myrajarenga <myrajarenga1234@gmail.com>
2023-03-01 23:45:08 -0800

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,7 +10,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height, right_height;
 
-	if  (tree == NULL)
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 	{
 		return (0);
